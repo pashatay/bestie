44f75f83d9e632eaf5e40189933c63e42d95855a
Pasha Tay <pashabonilla@gmail.com>
2020-01-03 00:31:48 -0500

    .

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 export default {
-  API_ENDPOINT:
-    "postgres://aqojmcpaewtnoo:aa0f8365419c1b47ce224a0b89ca44be8cc6bd23bad89fefaba5e1a4f174a439@ec2-174-129-33-230.compute-1.amazonaws.com:5432/d3gv9pn9jdeknn"
+  API_ENDPOINT: "https://fierce-hamlet-53635.herokuapp.com"
 };
